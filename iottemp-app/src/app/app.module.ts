a9492ebc43dbc324641ce5bf034e22476ad8ff9e
Bresenham <maximilian.gaul@googlemail.com>
2021-08-23 14:16:43 +0200

    Added data-table component, added Material-Tab to app.component.html to switch between graph and table data representation

--- iottemp-app/src/app/app.module.ts
+++ iottemp-app/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './ui/header/header.component';
 import { AppComponent } from './app.component';
 import { GraphComponent } from './ui/graph/graph.component';
+import { DataTableComponent } from './ui/data-table/data-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    GraphComponent
+    GraphComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
--- iottemp-app/src/app/material.module.ts
+++ iottemp-app/src/app/material.module.ts
@@ -4,19 +4,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu'
 import { MatCardModule } from '@angular/material/card'
 import { MatToolbarModule } from '@angular/material/toolbar'
-  
+import { MatTabsModule } from '@angular/material/tabs'
+
 @NgModule({
     imports: [
         MatButtonModule,
         MatMenuModule,
         MatCardModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatTabsModule
     ],
     exports: [
         MatButtonModule,
         MatMenuModule,
         MatCardModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatTabsModule
     ]
 })
 export class MaterialModule {}
