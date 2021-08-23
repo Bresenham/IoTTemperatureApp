8f4855bd1b1bb193ff9ee0eb38fc30c0dd027e4b
Bresenham <maximilian.gaul@googlemail.com>
2021-08-23 16:19:37 +0200

    Added filter button

--- iottemp-app/src/app/app.module.ts
+++ iottemp-app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgxEchartsModule } from 'ngx-echarts';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './ui/header/header.component';
 import { AppComponent } from './app.component';
@@ -10,19 +12,23 @@ import { GraphComponent } from './ui/graph/graph.component';
 import { DataTableComponent } from './ui/data-table/data-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FilterComponent } from './ui/header/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GraphComponent,
-    DataTableComponent
+    DataTableComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
     NgxEchartsModule.forRoot({
       /**
        * This will import all modules from echarts.
--- iottemp-app/src/app/material.module.ts
+++ iottemp-app/src/app/material.module.ts
@@ -6,6 +6,12 @@ import { MatCardModule } from '@angular/material/card'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatTabsModule } from '@angular/material/tabs'
 import { MatTableModule } from '@angular/material/table'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list'
 
 @NgModule({
     imports: [
@@ -14,7 +20,13 @@ import { MatTableModule } from '@angular/material/table'
         MatCardModule,
         MatToolbarModule,
         MatTabsModule,
-        MatTableModule
+        MatTableModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatGridListModule
     ],
     exports: [
         MatButtonModule,
@@ -22,7 +34,13 @@ import { MatTableModule } from '@angular/material/table'
         MatCardModule,
         MatToolbarModule,
         MatTabsModule,
-        MatTableModule
+        MatTableModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatGridListModule
     ]
 })
 export class MaterialModule {}
