49026e7c526829254e3b5465ce7457976731df77
Bresenham <maximilian.gaul@googlemail.com>
2021-08-25 15:17:46 +0200

    Added mongoDB docker compose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose');
 
+// npm start
 // docker run -d -p 27017:27017 --name m1 mongo
 
 mongoose.connect("mongodb://127.0.0.1:27017/",{
         useCreateIndex:true,
         useNewUrlParser: true,
         useUnifiedTopology: true}).then(()=> {
-console.log('Database Successfully Connected')},error =>{
-console.log(error)})
+          console.log('Database Successfully Connected')},
+          error =>{
+            console.log(error)}
+        );
 
 const app = express()
 const port = 3000
