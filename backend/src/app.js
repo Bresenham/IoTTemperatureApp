5ddfb023c5db9ef702c86de05fd41efbaf7130a5
Bresenham <maximilian.gaul@googlemail.com>
2021-08-28 15:54:30 +0200

    CORS is working now

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,18 +17,26 @@ mongoose.connect("mongodb://127.0.0.1:27017/",{
 const app = express()
 const port = 3000
 
+// Set up CORS
+app.use(cors({
+    origin: true, // "true" will copy the domain of the request back
+                  // to the reply. If you need more control than this
+                  // use a function.
+
+    credentials: true, // This MUST be "true" if your endpoint is
+                       // authenticated via either a session cookie
+                       // or Authorization header. Otherwise the
+                       // browser will block the response.
+
+    methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                           // pre-flight OPTIONS requests
+}));
+
 const sensorRouter = require('./routes/sensors');
 const dataRouter = require('./routes/data');
 
 app.use('/sensor', sensorRouter);
 app.use('/data', dataRouter);
-
-var corsOptions = {
-  origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
