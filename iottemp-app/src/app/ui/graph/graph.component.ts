d2ff2739493fed9c73320e17fb162cdb228ce454
Bresenham <maximilian.gaul@googlemail.com>
2021-09-06 16:42:08 +0200

    Added echart tooltip

--- iottemp-app/src/app/ui/graph/graph.component.ts
+++ iottemp-app/src/app/ui/graph/graph.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
+import { TooltipComponentOption } from 'echarts/components'
 
 import { DataEntry } from 'src/app/types/DataEntry';
 import { DataService } from '../../services/data.service';
+
+type TooltipFormatterCallback = Exclude<NonNullable<TooltipComponentOption['formatter']>, string>
+// single and multiple params
+type TooltipFormatterParams = Parameters<TooltipFormatterCallback>[0]
+
+const dataFormatter: TooltipFormatterCallback = (params: TooltipFormatterParams) => {
+  if (Array.isArray(params)) {
+    return params[0].data.toString() || 'no value';
+  }
+  return params.data.toString() || '';
+}
+
 
 @Component({
   selector: 'app-graph',
@@ -31,6 +44,9 @@ export class GraphComponent implements OnInit {
         },
         yAxis: {
           type: 'value',
+        },
+        tooltip: {
+          formatter: dataFormatter
         },
         series: [
           {
